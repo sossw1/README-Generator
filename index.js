71150b085efa11225dcdfd9a7966f7291b4cc509
sossw1 <sossw1@gmail.com>
2021-01-09 18:45:56 -0500

    Added questions formatted for Inquirer

--- index.js
+++ index.js
@@ -4,7 +4,38 @@ const fs = require('fs');
 const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of this project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe this project?',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'How would a new user install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this project to be used for?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can other developers contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How can other developers test this project?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
