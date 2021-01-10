d0c0c34c047a0b54e82cd62fa4d5107e2d42882e
sossw1 <sossw1@gmail.com>
2021-01-10 14:55:53 -0500

    Render license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,7 +51,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if(license === 'none') {
+    return '';
+  } else {
+    return `This project and all of its source code are subject to the ${license} license.`;
+  }
 }
 
 // TODO: Create a function to generate markdown for README
